9084a67a6170ae4d50e88e7abedf2018270486a6
Brian Azizi <brian.azizi@cazoo.co.uk>
2022-06-04 09:57:56 +0100

    Finish rust assembly-line

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,9 +3,23 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    return error_rate(speed) * cars_produced(speed);
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    return production_rate_per_hour(speed) as u32 / 60;
+}
+
+fn error_rate(speed: u8) -> f64 {
+    if speed >= 9 {
+        return 0.77;
+    } else if speed >= 5 {
+        return 0.9;
+    }
+
+    return 1.0;
+}
+
+fn cars_produced(speed: u8) -> f64 {
+    return 221.0 * speed as f64;
 }
