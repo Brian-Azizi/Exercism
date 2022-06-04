b036fbc6549c1fe81d353e75372269c46dabb3b2
Brian Azizi <brian.azizi@cazoo.co.uk>
2022-06-04 23:10:15 +0100

    Implement value_to_color_string

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,37 +1,32 @@
-#[derive(Debug, PartialEq)]
+use enum_iterator::IntoEnumIterator;
+use int_enum::IntEnum;
+
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, Eq, IntoEnumIterator, PartialEq, IntEnum)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Blue = 6,
+    Brown = 1,
+    Green = 5,
+    Grey = 8,
+    Orange = 3,
+    Red = 2,
+    Violet = 7,
+    White = 9,
+    Yellow = 4,
 }
 
 pub fn color_to_value(color: ResistorColor) -> usize {
-    match color {
-        ResistorColor::Black => 0,
-        ResistorColor::Brown => 1,
-        ResistorColor::Red => 2,
-        ResistorColor::Orange => 3,
-        ResistorColor::Yellow => 4,
-        ResistorColor::Green => 5,
-        ResistorColor::Blue => 6,
-        ResistorColor::Violet => 7,
-        ResistorColor::Grey => 8,
-        ResistorColor::White => 9,
-    }
+    color.int_value().into()
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    match ResistorColor::into_enum_iter().find(|color| color.int_value() as usize == value) {
+        Some(color) => {
+            format!("{:?}", color)
+        }
+        None => String::from("value out of range"),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
