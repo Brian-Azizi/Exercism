35886a4f0d7c2cedce3c7a86e5d7cbb7675f5a80
Brian Azizi <brian.azizi@cazoo.co.uk>
2022-06-05 00:54:59 +0100

    Implement exercise

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,16 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut magazine_map = HashMap::new();
+    for word in magazine {
+        let word_counter = magazine_map.entry(word).or_insert(0);
+        *word_counter += 1;
+    }
+
+    for word in note {
+        let word_counter = magazine_map.entry(word).or_insert(-1);
+        *word_counter -= 1;
+    }
+
+    magazine_map.iter().all(|(key, item)| *item >= 0)
 }
