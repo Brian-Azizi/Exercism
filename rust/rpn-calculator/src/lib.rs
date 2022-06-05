bc7f1d25e30990dff30d6afcdb319f6209d48df3
Brian Azizi <brian.azizi@cazoo.co.uk>
2022-06-05 01:17:00 +0100

    Implement

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -8,8 +8,34 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    unimplemented!(
-		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
-		inputs,
-	);
+    use CalculatorInput::*;
+
+    let mut stack: Vec<i32> = vec![];
+
+    for entry in inputs {
+        match entry {
+            Value(num) => stack.push(*num),
+            _ => {
+                let operation = match entry {
+                    Add => |x, y| x + y,
+                    Subtract => |x, y| x - y,
+                    Multiply => |x, y| x * y,
+                    Divide => |x, y| x / y,
+                    _ => panic!("This can't happen"),
+                };
+
+                let b = stack.pop();
+                let a = stack.pop();
+                match a.zip(b) {
+                    Some((first, second)) => stack.push(operation(first, second)),
+                    None => (break),
+                }
+            }
+        }
+    }
+
+    match stack.len() {
+        1 => Some(stack[0]),
+        _ => None,
+    }
 }
