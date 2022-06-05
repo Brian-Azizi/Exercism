0b63309130f5c8aaa4768b1ba8cc1b42dbaa4710
Brian Azizi <brian.azizi@cazoo.co.uk>
2022-06-05 10:49:59 +0100

    Implement clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,46 @@
-pub struct Clock;
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hour: i32,
+    minute: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let (hour, minute) = unwind_clock(hours, minutes);
+
+        Clock { hour, minute }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         unimplemented!("Add {} minutes to existing Clock time", minutes);
     }
+
+    pub fn to_string(&self) -> String {
+        return format!("{:0>2}:{:0>2}", self.hour, self.minute);
+    }
+}
+
+fn unwind_clock(hours: i32, minutes: i32) -> (i32, i32) {
+    let mut hour = hours;
+    let mut minute = minutes;
+
+    while minute < 0 {
+        hour -= 1;
+        minute += 60;
+    }
+
+    while minute >= 60 {
+        hour += 1;
+        minute -= 60;
+    }
+
+    while hour < 0 {
+        hour += 24
+    }
+
+    while hour >= 24 {
+        hour -= 24
+    }
+
+    return (hour, minute);
 }
