ed09f6104d8632ee0d2efb7e3eeed632c534b8c1
Brian Azizi <brian.azizi@cazoo.co.uk>
2022-06-11 22:23:55 +0100

    Implement anagrams

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,29 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut result = HashSet::new();
+    for &anagram in possible_anagrams {
+        if is_anagram(word.to_lowercase(), anagram.to_lowercase()) {
+            result.insert(anagram);
+        }
+    }
+
+    return result;
+}
+
+fn is_anagram(word: String, anagram: String) -> bool {
+    if word == anagram {
+        return false;
+    }
+    if sort_word(word) == sort_word(anagram) {
+        return true;
+    }
+    return false;
+}
+
+fn sort_word(word: String) -> String {
+    let mut sorted_word_vec = word.chars().collect::<Vec<char>>();
+    sorted_word_vec.sort_unstable();
+    let sorted_word = sorted_word_vec.iter().collect();
+    return sorted_word;
 }
